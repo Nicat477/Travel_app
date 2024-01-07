35b452fb46f6e91acc76036c790de7d466ce9627
Nicat477 <nicatrzazad30@gmail.com>
2024-01-08 02:44:27 +0300

    Initial commit

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/screens/home_page.dart';
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image:DecorationImage(
+          image: AssetImage('images/bg.jpg'),
+          fit: BoxFit.cover,
+          opacity: 0.7
+        )
+      ),
+      child:Material(
+        color:Colors.transparent,
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 65,horizontal: 25),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              Text('Enjoy',style: TextStyle(
+                color:Colors.white,
+                fontSize: 35,
+                letterSpacing: 1.5,
+                fontWeight: FontWeight.bold
+              ),),
+              SizedBox(height:2),
+              Text('the world!',style: TextStyle(
+                color:Colors.white.withOpacity(0.9),
+                fontSize: 35,
+                fontWeight: FontWeight.w400,
+                letterSpacing: 1.5
+              ),),
+              SizedBox(height:12),
+              Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",style:TextStyle(
+                color:Colors.white.withOpacity(0.7),
+                letterSpacing: 1.2,
+                fontSize: 16
+              ) ,),
+              SizedBox(height: 30),
+              InkWell(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+                },
+                child: Ink(
+                  padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color:Colors.white,
+                    borderRadius: BorderRadius.circular(12)
+                  ),
+                  child: Icon(
+                    Icons.arrow_forward_ios,
+                    size: 20,
+                    color:Colors.black54
+                  ),
+                ),
+              )
+            ]),),
+        ),
+      )
+    );
+  }
+}
